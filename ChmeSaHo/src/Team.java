d522381b87e508e53cda8a98abbddbec1636c813
LukChmelar <LukChmelar@e74df574-0296-11df-aea7-c5489bcb1a2d>
2010-01-16 20:06:16 +0000

    Team si nepotrebuje pamatat zoznam zapasov ani ich vysledkov, vsetko toto si uz pamata trieda Liga, je to teda zbytocne. Dalej som upravil metodu getBody aby pracovala so zoznamom Zapasov, moze to byt lubovolny pocet zapasov, hoci aj jeded ci vsetky z ligy, tato metoda vypocita pocet podov ktore za ne ziskaly.

--- ChmeSaHo/src/Team.java
+++ ChmeSaHo/src/Team.java
@@ -3,27 +3,34 @@ import java.util.List;
 
 
 public class Team {
-	String nazov;
-	int idTeamu;
+	private String nazov;
+	private int idTeamu;
 	List<Hrac> zoznamHracov;
-	List<Integer> vysledky; // -1 prehra, 0 remiza, 1 vyhra
-	List<Zapas> zoznamZapasov;
 	public Team(String nazov, int idTeamu) {
 		this.nazov=nazov;
 		this.idTeamu=idTeamu;
 		this.zoznamHracov=new ArrayList<Hrac>();
-		this.vysledky=new ArrayList<Integer>();
-		this.zoznamZapasov=new ArrayList<Zapas>();
 	}
-	public int getBody() {
+	public int getBody(List<Zapas> zapasy) {
 		int body=0;
-		for (int i = 0; i < vysledky.size(); i++) {
-			if(vysledky.get(i)==0) {
-				body+=1;
+		for (int i = 0; i < zapasy.size(); i++) {
+			if(zapasy.get(i).getIdTeam1()==idTeamu){
+				if(zapasy.get(i).getVysledok()==0) {
+					body+=1;
+				}
+				if(zapasy.get(i).getVysledok()==1) { // ak vyhral team 1
+					body+=3;
+				}
 			}
-			if(vysledky.get(i)==1) {
-				body+=3;
+			if(zapasy.get(i).getIdTeam2()==idTeamu){
+				if(zapasy.get(i).getVysledok()==0) {
+					body+=1;
+				}
+				if(zapasy.get(i).getVysledok()==2) { // ak vyhral team 2
+					body+=3;
+				}
 			}
+			
 		}
 		return body;
 	}
@@ -40,5 +47,17 @@ public class Team {
 			sucet+=h.getInkasGolov();
 		}
 		return sucet;
+	}
+	public String getNazov() {
+		return nazov;
+	}
+	public void setNazov(String nazov) {
+		this.nazov = nazov;
+	}
+	public int getIdTeamu() {
+		return idTeamu;
+	}
+	public void setIdTeamu(int idTeamu) {
+		this.idTeamu = idTeamu;
 	}
 }
