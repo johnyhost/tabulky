23b821a572b3fb0919fe46025517b206efaa62c5
LukChmelar <LukChmelar@e74df574-0296-11df-aea7-c5489bcb1a2d>
2010-01-17 15:41:22 +0000

    Premenovanie niektorych premmennych aby sa zhodovali s ostatnymi triedami

--- ChmeSaHo/src/Zapas.java
+++ ChmeSaHo/src/Zapas.java
@@ -1,13 +1,16 @@
 
 public class Zapas {
 	private int idZapasu;
-	private int idTeam1;
-	private int idTeam2;
+	private int idTeamu1;
+	private int idTeamu2;
 	private int vysledok; // 0 remiza, 1 vyhral idTeam1, 2 vyhral idTeam2
 	public Zapas(int idZapasu, int idTeam1, int idTeam2) {
 		this.idZapasu = idZapasu;
-		this.idTeam1 = idTeam1;
-		this.idTeam2 = idTeam2;
+		this.idTeamu1 = idTeam1;
+		this.idTeamu2 = idTeam2;
+	}
+	public Zapas(){
+		
 	}
 	public int getIdZapasu() {
 		return idZapasu;
@@ -15,17 +18,17 @@ public class Zapas {
 	public void setIdZapasu(int idZapasu) {
 		this.idZapasu = idZapasu;
 	}
-	public int getIdTeam1() {
-		return idTeam1;
+	public int getIdTeamu1() {
+		return idTeamu1;
 	}
-	public void setIdTeam1(int idTeam1) {
-		this.idTeam1 = idTeam1;
+	public void setIdTeamu1(int idTeamu1) {
+		this.idTeamu1 = idTeamu1;
 	}
-	public int getIdTeam2() {
-		return idTeam2;
+	public int getIdTeamu2() {
+		return idTeamu2;
 	}
-	public void setIdTeam2(int idTeam2) {
-		this.idTeam2 = idTeam2;
+	public void setIdTeamu2(int idTeamu2) {
+		this.idTeamu2 = idTeamu2;
 	}
 	public int getVysledok() {
 		return vysledok;
