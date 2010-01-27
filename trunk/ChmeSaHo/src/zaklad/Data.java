0d531399d289d1904001da2e0c5f01eabe4911f4
LukChmelar <LukChmelar@e74df574-0296-11df-aea7-c5489bcb1a2d>
2010-01-27 20:40:02 +0000

    Pridane prazdne metody na export do roznych formatov, treba doplnit kod

--- trunk/ChmeSaHo/src/zaklad/Data.java
+++ trunk/ChmeSaHo/src/zaklad/Data.java
@@ -18,4 +18,23 @@ public class Data {
 		
 		
 	}
+	public void exportHraciHTML(String filename){
+		// TODO metoda pre export hracov do HTML, treba teda vytvorit subor filename
+		// je zabezpecene aby subor obsahoval .htm resp. html, o to sa tu teda netreba starat
+	}
+	public void exportTeamyHTML(String filename){
+		// TODO metoda pre export teamov do HTML, treba teda vytvorit subor filename
+		// je zabezpecene aby subor obsahoval .htm resp. html, o to sa tu teda netreba starat
+	}
+	public void exportZapasyHTML(String filename){
+		// TODO metoda pre export zapasov do HTML, treba teda vytvorit subor filename
+		// je zabezpecene aby subor obsahoval .htm resp. html, o to sa tu teda netreba starat
+	}
+	public void exportSQL(String cesta){
+		// TODO metoda pre export ligy do SQL, cesta vyzera nasledovne
+		// X:\\Adresar\\ktory\\uzivatel\\zvolil\\nazovligy
+		// treba teda vytvorit subory X:\\Adresar\\ktory\\uzivatel\\zvolil\\nazovligy_teamy.sql
+		// X:\\Adresar\\ktory\\uzivatel\\zvolil\\nazovligy_hraci.sql
+		// X:\\Adresar\\ktory\\uzivatel\\zvolil\\nazovligy_zapasy.sql
+	}
 }
