17036b96c8eeef6660ecf1e544c19531242a8d4d
mirak.dva <mirak.dva@e74df574-0296-11df-aea7-c5489bcb1a2d>
2010-01-28 20:58:29 +0000

    pridanie parametra dlzka zapasu

--- trunk/ChmeSaHo/src/porovnavanie/BrankarByStats.java
+++ trunk/ChmeSaHo/src/porovnavanie/BrankarByStats.java
@@ -11,9 +11,9 @@ public class BrankarByStats implements Comparator<Hrac> {
 		this.liga = liga;
 	}
 	public int compare(Hrac brankar1, Hrac brankar2) {
-		if (brankar1.getPriemer(45)>brankar2.getPriemer(45)){
+		if (brankar1.getPriemer(liga.getDlzkaZapasu())>brankar2.getPriemer(liga.getDlzkaZapasu())){
 			return 1; 
-		} else if(brankar1.getPriemer(45)==brankar2.getPriemer(45)){			
+		} else if(brankar1.getPriemer(liga.getDlzkaZapasu())==brankar2.getPriemer(liga.getDlzkaZapasu())){			
 			if(brankar1.getOdchytMin()>brankar2.getOdchytMin()) {
 				return 1;
 			} else return -1;			
