cd7c369dbe411cf742a50541932d8c9dee6daea0
mirak.dva <mirak.dva@e74df574-0296-11df-aea7-c5489bcb1a2d>
2010-02-12 10:56:55 +0000

    ziadn velke zmeny, vyhodenie TO-DO riadkov

--- ChmeSaHo/src/zaklad/LigaMain.java
+++ ChmeSaHo/src/zaklad/LigaMain.java
@@ -6,12 +6,14 @@ public class LigaMain {
 
 	
 	public static void main(String[] args) throws IOException {
-		Liga league = new Liga();
-		/*Data data = new Data();
+		Liga league /*= new Liga()*/;
+		Data data = new Data();
 		league = data.loadXML("Data\\liga1.xml");
 		System.out.println(league.getZoznamTeamov().get(0).getZoznamHracov().get(0).getMeno());
-		*/
-		league.generateZapasy(5,0);
+		for (Zapas z : league.getZoznamZapasov()) {
+			System.out.println("Zapas s id "+z.getIdZapasu()+" je medzi: "+league.getTeam(z.getIdTeamu1()).getNazov()+" ("+z.getIdTeamu1()+") vs. "+league.getTeam(z.getIdTeamu2()).getNazov()+"("+z.getIdTeamu2()+")");
+		}
+		//league.generateZapasy(5,0);
 		
 		/*
 		data = new Data(league);
